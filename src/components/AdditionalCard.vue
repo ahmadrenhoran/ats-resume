f294e11325ba93e603c743bbf0644b47cf238ceb
Ahmad Maulana R <liezkaal@gmail.com>
2025-02-11 13:42:53 +0700

    update color and additional section ui

---
+++ src/components/AdditionalCard.vue
@@ -0,0 +1,50 @@
+<template>
+    <div 
+        @click="onClick"
+        class="group flex justify-start items-center border w-fit p-4 rounded border-gray-200 transition duration-300 cursor-pointer"
+        :class="{'bg-gray-200': isActive, 'hover:bg-gray-200': !isActive}"
+    >
+        <div class="flex flex-col items-center text-center gap-2">
+            <div class="relative">
+                <img 
+                    :src="imgPath" 
+                    :alt="alt" 
+                    width="150" 
+                    height="100"
+                    class="transition duration-300"
+                    :class="{'opacity-50': isActive, 'group-hover:opacity-50': !isActive}"
+                >
+                <div 
+                    class="absolute inset-0 bg-gray-200 opacity-0 transition duration-300" 
+                    :class="{'opacity-50': isActive, 'group-hover:opacity-50': !isActive}"
+                ></div>
+
+                <!-- Ikon ceklis abu-abu -->
+                <div 
+                    v-if="isActive"
+                    class="absolute top-1 right-1 bg-gray-400 text-white rounded-full p-1 flex items-center justify-center"
+                >
+                    ✔
+                </div>
+            </div>
+            <p 
+                class="font-semibold text-sm mt-2 font-mono transition duration-300"
+                :class="{'opacity-50': isActive, 'group-hover:opacity-50': !isActive}"
+            >
+                {{ title }}
+            </p>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+defineProps({
+    imgPath: String,
+    alt: String,
+    title: String,
+    isActive: Boolean,
+    onClick: Function,
+});
+</script>
--- src/components/FormResume.vue
+++ src/components/FormResume.vue
@@ -1,63 +1,94 @@
 <template>
     <div class="container m-auto max-w-2xl py-24">
         <div class="grid grid-cols-2 gap-4">
-            <h1 class="text-xl font-semibold col-span-2">Your Details</h1>
+            <div class="col-span-2">
+                <h1 class="text-xl font-semibold">Your Details</h1>
+                <p class="text-gray-500 text-sm">
+                    Include your basic information to help employers get to know you better.
+                </p>
+            </div>
             <div class="col-span-2">
                 <label for="job-title" class="block text-sm font-semibold text-gray-900 mb-1">Job Title</label>
                 <input type="text" id="job-title" placeholder="Enter Job Title"
-                    class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-gray-300 placeholder:text-gray-400 focus:outline-indigo-600">
+                    class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-gray-300 placeholder:text-gray-400 focus:outline-amber-500">
             </div>
             <div class="col-span-1">
                 <label for="first-name" class="block text-sm font-semibold text-gray-900 mb-1">First Name</label>
                 <input type="text" id="first-name" autocomplete="given-name" placeholder="Enter First Name"
-                    class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-gray-300 placeholder:text-gray-400 focus:outline-indigo-600">
+                    class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-gray-300 placeholder:text-gray-400 focus:outline-amber-500">
             </div>
             <div class="col-span-1">
                 <label for="last-name" class="block text-sm font-semibold text-gray-900 mb-1">Last Name</label>
                 <input type="text" id="last-name" autocomplete="family-name" placeholder="Enter Last Name"
-                    class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-gray-300 placeholder:text-gray-400 focus:outline-indigo-600">
+                    class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-gray-300 placeholder:text-gray-400 focus:outline-amber-500">
             </div>
             <div class="col-span-1">
                 <label for="email" class="block text-sm font-semibold text-gray-900 mb-1">Email</label>
                 <input type="email" id="email" autocomplete="email" placeholder="Enter Email"
-                    class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-gray-300 placeholder:text-gray-400 focus:outline-indigo-600">
+                    class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-gray-300 placeholder:text-gray-400 focus:outline-amber-500">
             </div>
             <div class="col-span-1">
                 <label for="location" class="block text-sm font-semibold text-gray-900 mb-1">Location</label>
                 <input type="text" id="location" autocomplete="address-level1" placeholder="Enter Location"
-                    class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-gray-300 placeholder:text-gray-400 focus:outline-indigo-600">
+                    class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-gray-300 placeholder:text-gray-400 focus:outline-amber-500">
             </div>
             <div class="col-span-2">
                 <label for="bio" class="block text-sm font-semibold text-gray-900 mb-1">Bio</label>
                 <textarea id="bio" rows="4" placeholder="Enter Bio"
-                    class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-gray-300 placeholder:text-gray-400 focus:outline-indigo-600"></textarea>
+                    class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-gray-300 placeholder:text-gray-400 focus:outline-amber-500"></textarea>
             </div>
         </div>
         <hr class="mt-8 mb-8 border-gray-300">
         <div class="grid grid-cols-1 gap-4">
-            <h1 class="text-xl font-semibold col-span-1">Experience</h1>
+            <div>
+                <h1 class="text-xl font-semibold col-span-1">Experience</h1>
+                <p class="text-gray-500 text-sm">
+                    List your work history, including roles and responsibilities in each position.
+                </p>
+            </div>
             <div ref="experienceContainer" class="grid gap-4">
                 <Experience v-for="(experience, index) in experiences" :key="index" :experience="experience"
                     :index="index" :deleteExp="() => deleteExp(index)" />
             </div>
             <button @click="addExp"
-                class="border border-indigo-600 hover:bg-indigo-700 hover:text-white text-sm py-3 rounded-xl transition delay-150">
+                class="border border-amber-500 hover:bg-amber-500 hover:text-white text-sm py-3 rounded-xl transition delay-150">
                 Add Experience
             </button>
         </div>
         <hr class="mt-8 mb-8 border-gray-300">
         <div class="grid grid-cols-1 gap-4">
-            <h1 class="text-xl font-semibold col-span-1">Education</h1>
+            <div>
+                <h1 class="text-xl font-semibold col-span-1">Education</h1>
+                <p class="text-gray-500 text-sm">
+                    Provide details about your educational background, including schools and degrees earned.
+                </p>
+            </div>
+
             <div ref="educationContainer" class="grid gap-4">
                 <Education v-for="(education, index) in educations" :key="index" :education="education"
                     :index="index" :deleteEdu="() => deleteEdu(index)" />
             </div>
             <button @click="addEdu"
-                class="border border-indigo-600 hover:bg-indigo-700 hover:text-white text-sm py-3 rounded-xl transition delay-150">
+                class="border border-amber-500 hover:bg-amber-500 hover:text-white text-sm py-3 rounded-xl transition delay-150">
                 Add Education
             </button>
         </div>
         <hr class="mt-8 mb-8 border-gray-300">
+        <div class="grid grid-cols-1 gap-4">
+            <div>
+                <h1 class="text-xl font-semibold col-span-1">Additional Sections</h1>
+                <p class="text-gray-500 text-sm">
+                    These are all optional sections you might want to add to fulfill your CV
+                </p>
+            </div>
+            <div class="grid grid-cols-3 gap-y-5">
+                <Additional :imgPath="'/images/elearning.png'" :alt="'Language'" :title="'Languages'" :isActive="selectLangSection"  :onClick="() => selectLangSection = !selectLangSection"/>
+                <Additional :imgPath="'/images/hardskill.png'" :alt="'Hard Skills'" :title="'Hard Skills'" :isActive="selectHardSklSection" :onClick="() => selectHardSklSection = !selectHardSklSection"/>
+                <Additional :imgPath="'/images/softskill.png'" :alt="'Soft Skills'" :title="'Soft Skills'" :isActive="selectSoftSklSection" :onClick="() => selectSoftSklSection = !selectSoftSklSection"/>
+                <Additional :imgPath="'/images/certificate.png'" :alt="'Certificates'" :title="'Certificates'" :isActive="selectCertSection" :onClick="() => selectCertSection = !selectCertSection"/>
+                <Additional :imgPath="'/images/social.png'" :alt="'Socials'" :title="'Website & Socials'" :isActive="selectSocialSection" :onClick="() => selectSocialSection = !selectSocialSection"/>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -69,6 +100,7 @@
     import autoAnimate from '@formkit/auto-animate';
     import Experience from './ExperienceField.vue';
     import Education from './EducationField.vue';
+    import Additional from './AdditionalCard.vue';
 
     const experiences = ref([{
         title: '',
@@ -79,8 +111,25 @@
         country: '',
         description: ''
     }]);
-
     const experienceContainer = ref(null);
+
+    const educations = ref([{
+        school: '',
+        major: '',
+        degree: '',
+        startDate: '',
+        endDate: '',
+        city: '',
+        country: '',
+        description: ''
+    }]);
+    const educationContainer = ref(null);
+
+    const selectLangSection = ref(false);
+    const selectHardSklSection = ref(false);
+    const selectSoftSklSection = ref(false);
+    const selectCertSection = ref(false);
+    const selectSocialSection = ref(false);
 
     const addExp = () => {
         experiences.value.push({
@@ -97,19 +146,6 @@
     const deleteExp = (index) => {
         experiences.value.splice(index, 1);
     };
-
-    const educations = ref([{
-        school: '',
-        major: '',
-        degree: '',
-        startDate: '',
-        endDate: '',
-        city: '',
-        country: '',
-        description: ''
-    }]);
-
-    const educationContainer = ref(null);
 
     const addEdu = () => {
         educations.value.push({
